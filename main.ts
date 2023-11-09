7b8ab474d4f22f33839a57c7f879bd610590cd69
OscarGarciaArteaga <148896260+OscarGarciaArteaga@users.noreply.github.com>
2023-11-09 18:15:27 +0000

    changes-1

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
+    basic.showNumber(randint(0, 6))
     music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
 })
